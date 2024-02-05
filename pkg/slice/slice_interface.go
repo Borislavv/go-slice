222a39378e2347b3abedf7381d9f4e9c6ad0587e
Boris <Glazunov2142@icloud.com>
2024-02-05 15:45:00 +0300

    chore: pkg/slice/slice_interface.go

--- pkg/slice/slice_interface.go
+++ pkg/slice/slice_interface.go
@@ -1,1 +1,15 @@
 package slice
+
+type Interface[T any] interface {
+	New(len int, cap int) *Slice[T]
+	Append(items ...T)
+	Prepend(items ...T)
+	Pop() (item T, ok bool)
+	Shift() (item T, ok bool)
+	Sub(idx int) (item T, ok bool)
+	Get(idx int) (item T, ok bool)
+	Has(idx int) (ok bool)
+	Remove(idx int) (ok bool)
+	Len() int
+	Cap() int
+}
